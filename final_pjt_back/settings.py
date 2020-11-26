8d3df0dd47f231f891ee6db1b304396dce4ca076
jeong-yerim4898 <jinju151002@gmail.com>
2020-11-26 20:15:05 +0900

    commit back

--- final_pjt_back/settings.py
+++ final_pjt_back/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'finalback.herokuapp.com',
+    '127.0.0.1'
+]
 
 
 # Application definition
