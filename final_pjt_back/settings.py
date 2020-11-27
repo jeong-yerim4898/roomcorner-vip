7754d3ae9945d293325168e9f49d9e27ff705500
jeong-yerim4898 <jinju151002@gmail.com>
2020-11-27 15:40:07 +0900

    commit back

--- final_pjt_back/settings.py
+++ final_pjt_back/settings.py
@@ -29,7 +29,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = [
     'finalback.herokuapp.com',
     '127.0.0.1'
-    'https://please-stay-home.netlify.app'
+    'please-stay-home.netlify.app'
 ]
 
 
